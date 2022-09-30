27e9c59bfe46120e20c7ac467b732907935e2a0b
田中信秋 <sinshu.d@gmail.com>
2022-09-30 18:34:49 +0900

    SoundFont loader is done!

--- src/test.zig
+++ src/test.zig
@@ -1,7 +1,9 @@
 test
 {
+    _ = @import("test_timgm6mb_sample.zig");
     _ = @import("test_timgm6mb_preset.zig");
     _ = @import("test_timgm6mb_instrument.zig");
+    _ = @import("test_musescore_sample.zig");
     _ = @import("test_musescore_preset.zig");
     _ = @import("test_musescore_instrument.zig");
 }
